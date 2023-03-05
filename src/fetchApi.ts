e2b3fae1c08cfc173bcaab67ad98419776433c1e
Alex Didenko <alex.461919@yandex.ru>
2023-03-05 20:07:28 +0700

    done

---
+++ src/fetchApi.ts
@@ -0,0 +1,52 @@
+import fakeUsers from './fakeUsers';
+import { User } from './types';
+
+export interface FetchResult {
+  items: User[];
+  pageInfo: {
+    totalCount: number;
+    totalPages: number;
+    pageSize: number;
+    currentPage: number;
+    nextPage?: number;
+    prevPage?: number;
+    hasNext: boolean;
+    hasPrev: boolean;
+  };
+}
+
+const fakeFata = fakeUsers(100);
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function fetchPage(): Promise<FetchResult>;
+async function fetchPage(page: number, pageSize: number): Promise<FetchResult>;
+async function fetchPage(page = 1, pageSize = Number.MAX_SAFE_INTEGER): Promise<FetchResult> {
+  //
+  await sleep(500);
+
+  const totalCount = fakeFata.length;
+  const totalPages = Math.ceil(totalCount / pageSize);
+  const currentPage = page <= totalPages ? page : totalPages;
+  const offset = pageSize * (currentPage - 1);
+  const items = fakeFata.slice(offset, pageSize * currentPage);
+
+  const nextPage = currentPage < totalPages ? currentPage + 1 : null;
+  const prevPage = currentPage > 1 ? currentPage - 1 : null;
+
+  return {
+    items,
+    pageInfo: {
+      totalCount,
+      totalPages,
+      pageSize,
+      currentPage,
+      ...(nextPage ? { nextPage } : null),
+      ...(prevPage ? { prevPage } : null),
+      hasNext: nextPage !== null,
+      hasPrev: prevPage !== null,
+    },
+  };
+}
+
+export default fetchPage;
